2c8c8e4789f13bd1c343b3cc572487bedf475623
Daniel Bautista P <76835848+Danielbp019@users.noreply.github.com>
2024-04-01 04:37:20 -0500

    CRUD completo
    Por fin completo. Solo delete admiten peticiones por ruta y por consulta

--- controllers/ArticuloController.js
+++ controllers/ArticuloController.js
@@ -5,11 +5,27 @@ export class ArticuloController {
     static async getArticulos(req, res) {
         try {
             const [datos] = await ArticuloModel.query(
-                "SELECT id ,titulo ,cuerpo ,autor ,created_at ,updated_at FROM articulos;");
+                "SELECT id ,titulo ,cuerpo ,autor ,created_at ,updated_at FROM articulos;")
+                
             res.json(datos);
         } catch (error) {
-            console.log(error)
             res.status(500).json({ error: 'Ha ocurrido un error al obtener los artículos' })
+        }
+    }
+
+    static async getbyidArticulos(req, res) {
+        try {
+            const id = req.params.id || req.query.id
+            const [datos] = await ArticuloModel.query(
+                "SELECT id ,titulo ,cuerpo ,autor ,created_at ,updated_at FROM articulos WHERE id = ?", [id])
+
+            if (datos.length === 0) {
+                return res.status(404).json({ message: "No se encontro el articulo" })
+            }
+            res.json(datos)
+
+        } catch (error) {
+            res.status(500).json({ error: 'Ha ocurrido un error al obtener el artículo' })
         }
     }
 
@@ -17,13 +33,46 @@ export class ArticuloController {
         /* Desde postman los datos no se envian como parametros si no como body, con application/x-www-form-urlencoded activado y Content-Type: "application/json" en el header */
         try {
             const { titulo, cuerpo, autor } = req.body //o req.query para enviar por url pero es mejor el body.
-            const [rows] = await ArticuloModel.query(
+            const [datos] = await ArticuloModel.query(
                 "INSERT INTO articulos (titulo ,cuerpo ,autor) VALUES (?, ?, ?)", [titulo, cuerpo, autor])
-            res.status(201).json({ id: rows.insertId, titulo, cuerpo, autor })
+
+            res.status(201).json({ id: datos.insertId, titulo, cuerpo, autor })
         } catch (error) {
-            console.log(error)
             res.status(500).json({ error: 'Ha ocurrido un error al crear el artículo' })
         }
     }
 
+    static async putArticulos(req, res) {
+        try {
+            const id = req.params.id || req.query.id
+            const { titulo, cuerpo, autor } = req.body;
+            const [datos] = await ArticuloModel.query(
+                "UPDATE articulos SET titulo = IFNULL(?, titulo), cuerpo = IFNULL(?, cuerpo), autor = IFNULL(?, autor) WHERE id = ?", [titulo, cuerpo, autor, id])
+
+            if (datos.affectedRows === 0) {
+                return res.status(404).json({ message: "No se encontro el articulo" })
+            }
+            res.status(200).json({ message: "Articulo editado" })
+
+        } catch (error) {
+            res.status(500).json({ error: 'Ha ocurrido un error al editar el artículo' })
+        }
+    }
+
+    static async DeleteArticulos(req, res) {
+        try {
+            // Acepta ambas rutas por Parámetros de ruta (http://localhost:8000/api/articulos/13) y Parámetros de consulta (http://localhost:8000/api/articulos/?id=13). La ruta esta definida como /articulos/:id?
+            const id = req.params.id || req.query.id
+            const [datos] = await ArticuloModel.query(
+                "DELETE FROM articulos WHERE id = ?", [id])
+
+            if (datos.affectedRows === 0) {
+                return res.status(404).json({ message: "No se encontro el articulo" })
+            }
+            res.status(200).json({ message: "Articulo eliminado" })
+
+        } catch (error) {
+            res.status(500).json({ error: 'Ha ocurrido un error al obtener el artículo' })
+        }
+    }
 }
--- routes/ArticuloRouter.js
+++ routes/ArticuloRouter.js
@@ -1,10 +1,18 @@
 import { Router } from 'express'
 import { ArticuloController } from '../controllers/ArticuloController.js'
 
-export const articuloRouter = Router()//Se exporta y se declara la clase al mismo tiempo con este codigo
+export const articuloRouter = Router()// Se exporta y se declara la clase al mismo tiempo con este codigo
 
-//Siempre que se llaman rutas de este archivo comienzan con la constante articuloRouter.(tipo de ruta)
-articuloRouter.get('/', (req, res) => res.send('Ruta get del hola desde node y express js'))
+// Siempre que se llaman rutas de este archivo comienzan con la constante articuloRouter.(tipo de ruta)
+articuloRouter.get('/', (req, res) => res.send('Ruta del hola desde node y express js'))
 
+// GET todos
 articuloRouter.get('/articulos', ArticuloController.getArticulos)
+// GET uno solo
+articuloRouter.get('/articulos/:id?', ArticuloController.getbyidArticulos)
+// POST articulos
 articuloRouter.post('/articulos', ArticuloController.postCreateArticulos)
+// DELETE articulos
+articuloRouter.delete('/articulos/:id?', ArticuloController.DeleteArticulos)
+// PUT articulos
+articuloRouter.put('/articulos/:id?', ArticuloController.putArticulos);
